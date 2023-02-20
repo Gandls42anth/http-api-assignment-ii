aa584526312f4958741904c38e1db1dace42e5c5
Gandls42anth <70642446+Gandls42anth@users.noreply.github.com>
2023-02-19 20:40:27 -0500

    Add files via upload

--- src/responses.js
+++ src/responses.js
@@ -5,126 +5,104 @@ const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 
 const users = {};
 
-
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, { 'Content-Type': 'application/json' });
-    response.write(JSON.stringify(object));
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
-const respond = (request, response, content, type,code) => {
-    // set status code (200 success) and content type
-    response.writeHead(code, { 'Content-Type': type });
-    // write the content string or buffer to response
-    response.write(content);
-    // send the response to the client
-    response.end();
+const respond = (request, response, content, type, code) => {
+  // set status code (200 success) and content type
+  response.writeHead(code, { 'Content-Type': type });
+  // write the content string or buffer to response
+  response.write(content);
+  // send the response to the client
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-    //object for our headers
-    //Content-Type for json
-    const headers = {
-      'Content-Type': 'application/json',
-    };
-    //send response without json object, just headers
-    response.writeHead(status, headers);
-    debugger;
-    response.end();
-    //Somehow response.end on json.meta creates a get request for the same url with an empty query parameter
+  // object for our headers
+  // Content-Type for json
+  const headers = {
+    'Content-Type': 'application/json',
   };
+    // send response without json object, just headers
+  response.writeHead(status, headers);
 
-const getCSS = (request,response) => {
-    return respond(request,response,css,'text/css',200);
+  response.end();
 };
 
-const getIndex = (request,response) => {
-    debugger;
-    return respond(request,response,index,'text/html',200);
-};
+const getCSS = (request, response) => respond(request, response, css, 'text/css', 200);
+
+const getIndex = (request, response) => respond(request, response, index, 'text/html', 200);
 
 const getUsers = (request, response) => {
-    const responseJSON = {
-      users,
-    };
-  
-    respondJSON(request, response, 200, responseJSON);
+  const responseJSON = {
+    users,
   };
 
-const headUsers = (request,response) => {
-    debugger;
-    return respondJSONMeta(request, response, 200);
+  respondJSON(request, response, 200, responseJSON);
 };
 
-const getNotReal =  (request,response) =>{
-    debugger;
+const headUsers = (request, response) => respondJSONMeta(request, response, 200);
+
+const getNotReal = (request, response) => {
+  const responseJson = {
+    message: 'The page you are looking for was not found',
+  };
+  return respond(request, response, JSON.stringify(responseJson), 'application/json', 404);
+};
+
+const headNotReal = (request, response) => respondJSONMeta(request, response, 404);
+
+const addUser = (request, response, name, age) => {
+  if (!name || !age) {
     const responseJson = {
-        message: "The page you are looking for was not found"
-    }
-    return respond(request,response,JSON.stringify(responseJson),'application/json',404);
-};
-
-const headNotReal = (request,response) => {
-    debugger;
-    return respondJSONMeta(request,response,404);
-};
-
-const addUser = (request, response,name,age) => {
-    debugger;
-    
-    if(!name || !age){
-        debugger;
-        const responseJson = {
-            message: "Name and age are both required",
-        }
-        return respond(request,response,JSON.stringify(responseJson),'application/json',400);
-    }
-    
-    debugger;
-    //change to make to user
-    //This is just a dummy object for example
-    const newUser = {
-      age: age,
-      name: name
+      message: 'Name and age are both required',
     };
-  
-    // modifying our dummy object
-    // just indexing by time for now
-    debugger;
-    if(users[newUser.name] != null){
-        debugger;
-        //If someone with the name already exists, overwrite it and respond 204
-        users[newUser.name] = newUser;
-        debugger;
-        return respondJSONMeta(request, response, 204);
-        
-    }else{
-        //If they don't, it'll create one so respond 201
-        debugger;
+    return respond(request, response, JSON.stringify(responseJson), 'application/json', 400);
+  }
+
+  // change to make to user
+  // This is just a dummy object for example
+  const newUser = {
+    age,
+    name,
+  };
+
+  // modifying our dummy object
+  // just indexing by time for now
+
+  if (users[newUser.name] != null) {
+    // If someone with the name already exists, overwrite it and respond 204
     users[newUser.name] = newUser;
-    const responseJson = {
-        "Message" : "Created Successfully"
-    }
-    //return a 201 created status
-    return respondJSON(request, response, 201, responseJson);
-    }
+
+    return respondJSONMeta(request, response, 204);
+  }
+  // If they don't, it'll create one so respond 201
+
+  users[newUser.name] = newUser;
+  const responseJson = {
+    Message: 'Created Successfully',
+  };
+  // return a 201 created status
+  return respondJSON(request, response, 201, responseJson);
 };
 
-const notFound = (request,response) => {
-    debugger;
-    const responseJson = {
-        message: "The page you are looking for was not found"
-    }
-    return respond(request,response,JSON.stringify(responseJson),'application/json',404);
+const notFound = (request, response) => {
+  const responseJson = {
+    message: 'The page you are looking for was not found',
+  };
+  return respond(request, response, JSON.stringify(responseJson), 'application/json', 404);
 };
 
 module.exports = {
-    getCSS,
-    getIndex,
-    getNotReal,
-    headNotReal,
-    headUsers,
-    addUser,
-    getUsers,
-    notFound,
+  getCSS,
+  getIndex,
+  getNotReal,
+  headNotReal,
+  headUsers,
+  addUser,
+  getUsers,
+  notFound,
 };
--- src/server.js
+++ src/server.js
@@ -1,68 +1,64 @@
-const { debug } = require('console');
+const query = require('querystring');
 const http = require('http'); // http module
 const url = require('url'); // url module
-const responseHandler = require('./response.js');
+const responseHandler = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-// here we create a object to route our requests to the proper
-// handlers. the top level object is indexed by the request
-// method (get, head, etc). We can use request.method to return
-// the routing object for each type of method. Once we say
-// urlStruct[request.method], we recieve another object which
-// routes each individual url to a handler. We can index this
-// object in the same way we have used urlStruct before.
 const urlStruct = {
+  GET: {
+    '/': responseHandler.getIndex,
+    '/style.css': responseHandler.getCSS,
+    '/getUsers': responseHandler.getUsers,
+    '/notReal': responseHandler.getNotReal,
+    notFound: responseHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': responseHandler.headUsers,
+    '/notReal': responseHandler.headNotReal,
+    notFound: responseHandler.notFound,
 
-  '/': responseHandler.getIndex,
-  '/style.css': responseHandler.getCSS,
-  '/success': responseHandler.success,
-  '/badRequest': responseHandler.badRequest,
-  '/unauthorized': responseHandler.unauthorized,
-  '/forbidden': responseHandler.forbidden,
-  '/internal': responseHandler.internal,
-  '/notImplemented': responseHandler.notImplemented,
-  '/notFound': responseHandler.notFound,
-  notFound: responseHandler.notFound,
-
+  },
+  POST: {
+    '/addUser': responseHandler.addUser,
+    notFound: responseHandler.notFound,
+  },
 };
 
-// function to handle requests
 const onRequest = (request, response) => {
-  // first we have to parse information from the url
   const parsedUrl = url.parse(request.url);
 
-  const acceptedTypes = request.headers.accept.split(',');
-  // If accepted types does not exist, that means go to default
-  // If it does exist, use the first entry
-  if (acceptedTypes[0] != 'application/json' && acceptedTypes[0] != 'text/xml') {
-    acceptedTypes[0] = 'application/json';
+  if (!urlStruct[request.method]) {
+    return urlStruct.HEAD.notFound(request, response);
   }
-  const pref = acceptedTypes[0];
+  // If it's a head/get request don't bother with parameters just call the method
+  if (request.method === 'POST') {
+    const body = [];
+    request.on('error', () => {
+      response.statusCode = 400;
+      return response.end();
+    });
 
-  // check if the path name (the /name part of the url) matches
-  // any in our url object. If so call that function. If not, default to index.
-  if (parsedUrl.pathname == '/badRequest') {
-    if (parsedUrl.query == 'valid=true') {
-      urlStruct[parsedUrl.pathname](request, response, pref, true);
-    } else {
-      urlStruct[parsedUrl.pathname](request, response, pref, false);
-    }
-  } else if (parsedUrl.pathname == '/unauthorized') {
-    if (parsedUrl.query == 'loggedIn=yes') {
-      urlStruct[parsedUrl.pathname](request, response, pref, true);
-    } else {
-      urlStruct[parsedUrl.pathname](request, response, pref, false);
-    }
-  } else if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response, pref, true);
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const bS = Buffer.concat(body).toString();
+      const bP = query.parse(bS);
+      // Once we have the bodyParams object, we will call the handler function. We then
+      // proceed much like we would with a GET request.
+      return urlStruct[request.method][parsedUrl.pathname](request, response, bP.name, bP.age);
+    });
+  } else if (urlStruct[request.method][parsedUrl.pathname]) {
+    return urlStruct[request.method][parsedUrl.pathname](request, response);
   } else {
-    // otherwise send them to the index (normally this would be the 404 page)
-    urlStruct['/'](request, response, pref, true);
+    return urlStruct.GET['/'](request, response);
   }
+
+  return 1;
 };
 
 // start server
 http.createServer(onRequest).listen(port, () => {
-  console.log(`Listening on 127.0.0.1: ${port}`);
 });
